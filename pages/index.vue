03b7300431d70f318e911533364e8c1ddefa80d7
unknown <kennethgacho23>
2026-05-19 10:25:51 +0800

    navigation drawer

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,14 @@
     <v-app>
         <v-main>
             <v-container>
-                
+                <v-row>
+                    <v-col>
+                        <v-textfield
+                            label="STUDENT NO."
+                            variant="outlined">
+                        </v-textfield>
+                    </v-col>
+                </v-row>    
             </v-container>
         </v-main>
     </v-app>
